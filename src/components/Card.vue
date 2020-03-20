b5e5c2d2cf34a605bb0335a5f10dac7ad5d491fa
Paalar <paaledward@gmail.com>
2020-03-20 02:26:39 +0100

    Create card component

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="card">
+        <slot>
+        </slot>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'Card',
+});
+</script>
+
+<style lang="stylus" scoped>
+    .card
+        -webkit-box-shadow: 5px 10px 10px 5px rgba(0,0,0,0.27);
+        -moz-box-shadow: 5px 10px 10px 5px rgba(0,0,0,0.27);
+        box-shadow: 5px 10px 10px 5px rgba(0,0,0,0.27);
+
+        width 100%
+        background-color white
+        border-radius 15px
+        padding 3rem 3rem 3rem 3rem
+        margin-top 1 rem
+</style>
