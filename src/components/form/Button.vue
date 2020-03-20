5ffc58124e59bc645561f5c3e8ab59eb4b6e8219
Paalar <paaledward@gmail.com>
2020-03-20 23:39:12 +0100

    Add google svg and form components

---
+++ src/components/form/Button.vue
@@ -0,0 +1,36 @@
+<template>
+    <button
+        v-on:click="$emit('on-click')"
+        class="button"
+        :class="type"
+    >
+        {{ text }}
+    </button>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        type: String,
+        text: String,
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+@import '@/assets/css/common.sass'
+
+.button
+    border-radius: 25px
+    height: 25px
+    width: 75px
+    text-align: center
+
+.white
+    background-color: #fff
+    color: $red-main
+
+.red
+    background-color: $red-main
+    color: #fff
+</style>
---
+++ src/components/form/GoogleLogin.vue
@@ -0,0 +1,38 @@
+<template>
+    <button @click="signInWithGoogle">
+        <img src="@/assets/images/google-logo.svg" />
+        <span>Logg inn med google</span>
+    </button>
+</template>
+
+<script lang="ts">
+import { signInWithGoogle } from '@/firebase/functions';
+
+export default {
+    methods: {
+        signInWithGoogle,
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+@import '@/assets/css/common.sass'
+
+button
+    display: flex
+    align-items: center
+    color: #fff
+    background: $google-blue
+    margin: auto
+    -webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75)
+    -moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75)
+    box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75)
+    padding-right: 1rem
+    border: 1px solid $google-blue
+    font-weight: 500
+    font-size: 16px
+
+img
+    background: white
+    margin-right: 1rem
+</style>
