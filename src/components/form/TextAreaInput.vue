9d420abd4ede28525c869b6fc2b9327449de474e
Paalar <paaledward@gmail.com>
2020-03-21 12:58:31 +0100

    Abstract text area

---
+++ src/components/form/TextAreaInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="inputContainer">
+        <label v-if="hasLabel"><b>{{ label }}</b></label>
+        <textarea
+            wrap="hard"
+            placeholder="placeholder"
+            :value="value"
+            @input="$emit('input', $event.target.value)"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        value: String,
+        label: String,
+        placeholder: String,
+    },
+    methods: {
+        hasLabel() {
+            return this.label.length > 0;
+        },
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
