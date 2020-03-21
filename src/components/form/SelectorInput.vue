2457e1e9e3c34c8d9a653d24c375a5c539de8c71
Paalar <paaledward@gmail.com>
2020-03-21 12:58:13 +0100

    Abstract selector

---
+++ src/components/form/SelectorInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="inputContainer">
+        <label v-if="hasLabel"><b>{{ label }}</b></label>
+        <select :value="value" @input="setOption">
+            <option disabled value="" >{{ disabled }}</option>
+            <option v-for="city in cities" :key="city">{{ city }}</option>
+        </select>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        value: String,
+        cities: Array,
+        label: String,
+        disabled: String,
+    },
+    methods: {
+        hasLabel() {
+            return this.label.length > 0;
+        },
+        setOption(event: Event) {
+            const { selectedIndex } = event.explicitOriginalTarget.options;
+            const city = this.cities[selectedIndex - 1];
+            return this.$emit('input', city);
+        },
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
