74681a7590974f6b3f16f6b10d2ec71e419b871f
Paalar <paaledward@gmail.com>
2020-03-23 23:28:10 +0100

    Create a pofile view which redirects to other sub pages, and includes a nav bar

---
+++ src/views/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+    <page>
+        <template v-slot:nav>
+            <side-bar class="nav" />
+        </template>
+        <div>
+            <router-view></router-view>
+        </div>
+    </page>
+</template>
+
+<script lang=ts>
+import store from '@/store';
+import { fetchAllUserPosts } from '@/firebase/functions';
+import Page from '@/components/Page.vue';
+import SideBar from '@/components/SideBar.vue';
+
+export default {
+    name: 'Profile',
+    components: {
+        Page,
+        SideBar,
+    },
+    mounted() {
+        fetchAllUserPosts(store.getters.user);
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
