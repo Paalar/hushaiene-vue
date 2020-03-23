7c354f1fcca8d18b74c5527ce10543785f302ffc
Paalar <paaledward@gmail.com>
2020-03-23 23:26:59 +0100

    Fetch a users posts and display them

--- src/firebase/functions.ts
+++ src/firebase/functions.ts
@@ -24,6 +24,50 @@ export const fetchAllPosts = () => {
         });
 };
 
+// TODO change from strings to reference to get them fast
+export const fetchAllUserPosts = async (user) => {
+    firebaseConfig.postsCollection
+        .where('userId', '==', user.uid)
+        .get()
+        .then((snapshot) => {
+            const posts: Post[] = [];
+            snapshot.forEach((document) => {
+                const data = document.data();
+                const post = {
+                    id: document.id,
+                    ...data,
+                };
+                posts.push(post);
+            });
+            store.dispatch('profile/setPosts', posts);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    // userRef.onSnapshot(async (postRefSnapshot) => {
+    //     const documentData = postRefSnapshot.data();
+    //     await documentData.posts.forEach((documentSnapshot) => {
+    //         // const posts = [];
+    //         documentSnapshot.onSnapshot((post) => {
+    //             const data = post.data();
+    //             // posts.push(data);
+    //             store.dispatch('profile/setPosts', post.data());
+    //         });
+    //     });
+    // });
+};
+
+export const userExists = (user) => {
+    const userRef = firebaseConfig.usersCollection.doc(user.uid);
+    return userRef.get()
+        // Returns data if exists, else undefined
+        .then((doc) => doc.data())
+        .catch((error) => {
+            console.log(error);
+            // Error getting document
+        });
+};
+
 export const createNewPostPromise = (postData: NewPost) => (
     firebaseConfig.postsCollection.add({
         created: moment().format(),
@@ -33,6 +77,16 @@ export const createNewPostPromise = (postData: NewPost) => (
         userName: store.getters.user.displayName,
         ...postData,
     })
+        .then((result) => {
+            const userId = store.getters.user.uid;
+            const userRef = firebaseConfig.usersCollection.doc(userId);
+            userRef.update({
+                posts: firebase.firestore.FieldValue.arrayUnion(result),
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 );
 
 export const signInWithGoogle = () => firebase.auth().signInWithRedirect(googleProvider);
@@ -41,36 +95,51 @@ export const getRedirectFromGoogle = () => {
     if (store.getters.isLoggedIn) {
         router.push('/');
     }
-    firebase.auth().getRedirectResult().then(async (result) => {
-        if (result.credential) {
-            const OAuth = result.credential;
-            const { accessToken } = OAuth; // TODO cast to oauth to remove error
-            store.dispatch('setToken', accessToken);
-        }
-        const { user } = result;
-        if (user !== null) {
-            await store.dispatch('signIn', user);
-            router.push('/');
-        }
-    }).catch((error) => {
-        // TODO better error handling
-        window.alert('Kunne ikke logge inn');
-        console.log(error);
-        // Handle Errors here.
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // The email of the user's account used.
-        // const email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        // const credential = error.credential;
-        // ...
-    });
+    firebase.auth().getRedirectResult()
+        .then(async (result) => {
+            if (result.credential) {
+                const OAuth = result.credential;
+                const { accessToken } = OAuth; // TODO cast to oauth to remove error
+                store.dispatch('setToken', accessToken);
+            }
+            const { user } = result;
+            if (user !== null) {
+                const userData = await userExists(user);
+                if (userData === undefined) {
+                    await firebaseConfig.usersCollection.doc(user.uid).set({ posts: [] });
+                }
+                await store.dispatch('signIn', user);
+                router.push('/');
+            }
+        }).catch((error) => {
+            // TODO better error handling
+            window.alert('Kunne ikke logge inn');
+            console.log(error);
+            // Handle Errors here.
+            // const errorCode = error.code;
+            // const errorMessage = error.message;
+            // The email of the user's account used.
+            // const email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            // const credential = error.credential;
+            // ...
+        });
 };
 
-export const signOutPromise = () => firebase.auth().signOut();
+export const signOut = () => (
+    firebase.auth().signOut().then(async () => {
+        await store.dispatch('signOut');
+        window.location.assign('/');
+    }).catch((error) => {
+        console.log(error);
+        // sign out unsuccessfull
+        // TODO better error handling
+        window.alert('Kunne ikke logge ut');
+    })
+);
 
 export default {
     fetchAllPosts,
     signInWithGoogle,
-    signOutPromise,
+    signOut,
 };
---
+++ src/views/Profile/ProfilePosts.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <card>
+            <h1>Innlegg</h1>
+        </card>
+        <card
+            v-for="(post, name) in posts"
+            :key="name"
+        >
+            {{ post }}
+        </card>
+    </div>
+</template>
+
+<script lang="ts">
+import { mapState } from 'vuex';
+import Card from '@/components/Card.vue';
+
+export default {
+    components: {
+        Card,
+    },
+    computed: {
+        ...mapState('profile', {
+            posts: (state) => state.posts,
+            loaded: (state) => state.loaded,
+        }),
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
