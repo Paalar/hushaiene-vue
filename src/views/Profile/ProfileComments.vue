bdf1e00625b8207e0499cafaba4e274fa3deb1cf
Paalar <paaledward@gmail.com>
2020-03-23 23:27:32 +0100

    Create shell pages for comments and settings

---
+++ src/views/Profile/ProfileComments.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <card>
+            <h1>Kommentarer</h1>
+        </card>
+    </div>
+</template>
+
+<script lang="ts">
+import Card from '@/components/Card.vue';
+
+export default {
+    components: {
+        Card,
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
